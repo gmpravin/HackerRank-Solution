47d800ebb5828c67e40542095f36313640136cae
gmpravin <gmpravin79@gmail.com>
2020-03-21 00:45:24 +0530

    working

--- Java/loops2.java
+++ Java/loops2.java
@@ -3,12 +3,13 @@ import java.io.*;
 
 class loops2 {
     public static void main(String[] argh) {
-        Scanner s = new Scanner(System.in);
-        int t = s.nextInt();
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+
         for (int i = 0; i < t; i++) {
-            int a = s.nextInt();
-            int b = s.nextInt();
-            int n = s.nextInt();
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
 
             for (int j = 0; j < n; j++) {
                 a = a + (int) Math.pow(2, j) * b;
@@ -16,7 +17,7 @@ class loops2 {
             }
             System.out.println("");
         }
-        s.close();
+        in.close();
 
     }
 }
