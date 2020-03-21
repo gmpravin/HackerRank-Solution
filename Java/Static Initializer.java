b9eea3aea19c663dce7a246a8f270350f8ef690c
gmpravin <40927922+gmpravin@users.noreply.github.com>
2020-03-21 10:49:07 +0530

    working

---
+++ Java/Static Initializer.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+ static Scanner s = new Scanner(System.in);
+ static int B = s.nextInt();
+ static int H = s.nextInt();
+ static boolean flag;
+ static boolean res = B > 0 && H > 0;
+ static {
+  if (res) {
+   flag = true;
+  } else {
+   flag = false;
+   System.out.println("java.lang.Exception: Breadth and height must be positive");
+  }
+ }
+
+
+
+ public static void main(String[] args) {
+  if (flag) {
+   int area = B * H;
+   System.out.print(area);
+  }
+
+ } //end of main
+
+} //end of class
