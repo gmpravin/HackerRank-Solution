4fd39e601295097de8ded3af7bf8c91ab38a4bd4
gmpravin <40927922+gmpravin@users.noreply.github.com>
2020-03-21 11:01:06 +0530

    Working

--- Java/int to string.java
+++ Java/int to string.java
@@ -3,14 +3,12 @@ import java.security.*;
 public class Solution {
  public static void main(String[] args) {
 
-  DoNotTerminate.forbidExit();
 
   try {
    Scanner in = new Scanner(System.in);
    int n = in .nextInt();
    in.close();
- String s=Integer.toString(n);
-   
+   String s=Integer.toString(n);
    if (n == Integer.parseInt(s)) {
     System.out.println("Good job");
    } else {
@@ -22,22 +20,3 @@ public class Solution {
  }
 }
 
-class DoNotTerminate {
-
- public static class ExitTrappedException extends SecurityException {
-
-  private static final long serialVersionUID = 1;
- }
-
- public static void forbidExit() {
-  final SecurityManager securityManager = new SecurityManager() {
-   @Override
-   public void checkPermission(Permission permission) {
-    if (permission.getName().contains("exitVM")) {
-     throw new ExitTrappedException();
-    }
-   }
-  };
-  System.setSecurityManager(securityManager);
- }
-}
